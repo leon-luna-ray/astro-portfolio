e12594fa02d0d496595526f90aa91121fc4bab87
Ray Luna <leon.luna.ray@gmail.com>
2023-04-16 21:44:08 -0700

    setup base styles for tout

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,8 +5,10 @@ const props = defineProps({
 </script>
 <template>
     <div class="hero-banner">
-        <div class=" container inner">
-            <h1>🧑‍🚀 Hello, World!</h1>
+        <div class="container inner">
+            <div class="title">
+                <h1>👨🏻‍🚀 <span>Hello World!</span> 🌎</h1>
+            </div>
             <p>{{ intro }}</p>
         </div>
     </div>
