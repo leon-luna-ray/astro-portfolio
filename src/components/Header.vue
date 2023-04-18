aed39caf44c940d1e65b5f9402798f37e233534d
Ray Luna <leon.luna.ray@gmail.com>
2023-04-17 19:41:50 -0700

    add logo and bg assets

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,8 +6,8 @@ const props = defineProps({
 <template>
     <header id="site-header">
         <div class="container">
-            <div class="title">
-                <h2>{{ title }}</h2>
+            <div id="logo">
+                <img src="img/rl-logo-lw.png" alt="Ray Luna Full-stack web developer logo">
             </div>
             <nav>
                 <a href="https://github.com/leon-luna-ray" target="_blank">GitHub</a>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,34 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-		'play': ['Play', 'sans-serif'],
+        play: ['Play', 'sans-serif'],
+      },
+      backgroundImage: {
+        'blue-dots': "url('/img/loc-bg.jpg')",
+        'black-dots': "url('/img/black-tc.jpg')",
+        'color-stars': "url('/img/color-stars.jpg')",
+        'white-stars': "url('/img/white-stars.jpg')",
       },
       container: {
         center: true,
-        padding: "2.5rem",
+        padding: '2.5rem',
         screens: {
-          sm: "100%",
-          md: "100%",
-          lg: "100%",
-          xl: "1600px",
+          sm: '100%',
+          md: '100%',
+          lg: '100%',
+          xl: '1600px',
         },
       },
       colors: {
         primary: {
-          red: "#F8333C",
-          yellow: "#FDCA40",
-          green: "#44AF69",
-          blue: "#2B9EB3",
-          navy: "#1B2845",
+          red: '#F8333C',
+          yellow: '#FDCA40',
+          green: '#44AF69',
+          blue: '#2B9EB3',
+          navy: '#1B2845',
         },
         soft: {
-          white: "#F3F7F8",
-          black: "#202222",
+          white: '#F3F7F8',
+          black: '#202222',
         },
       },
     },
