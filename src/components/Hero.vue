b37f9bba298bef57521de69235a71e262df351f7
Ray Luna <leon.luna.ray@gmail.com>
2023-04-17 20:19:39 -0700

    hero banner styles

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,11 +5,12 @@ const props = defineProps({
 </script>
 <template>
     <div class="hero-banner">
-        <div class="container inner">
+        <div class="inner">
             <div class="title">
                 <h1>👨🏻‍🚀 <span>Hello World!</span> 🌎</h1>
             </div>
             <p>{{ intro }}</p>
+            <p>This Page is in developement</p>
         </div>
     </div>
 </template>
