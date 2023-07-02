287bb2f2c3d00f0bc56b0027a107f0f2223c17b0
Ray Luna <leon.luna.ray@gmail.com>
2023-07-02 10:53:13 -0700

    add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import IconEmail from './icons/IconEmail.vue';
+import IconGithub from './icons/IconGithub.vue';
+import IconLinkedin from './icons/IconLinkedin.vue';
+
 const year = new Date().getFullYear();
 </script>
 <template>
-    <footer id="site-footer">
-        <div class="dev">This Page is in development</div>
-        <div>&copy; {{ year }} Ray Luna</div>
+    <footer id="footer">
+        <div class='social-icons'>
+            <a href='#'>
+                <IconEmail />
+            </a>
+            <a href='#'>
+                <IconGithub />
+            </a>
+            <a href='#'>
+                <IconLinkedin />
+            </a>
+        </div>
+        <div class='copyright'>
+            <p>© {{ year }} Ray Luna. All rights reserved.</p>
+        </div>
     </footer>
 </template>
