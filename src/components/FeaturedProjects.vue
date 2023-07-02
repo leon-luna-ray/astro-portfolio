22a3d3be2510b35eb63fcfc648ca013bdb559b3b
Ray Luna <leon.luna.ray@gmail.com>
2023-07-02 00:42:24 -0700

    add featured projects cards to grid

--- src/components/FeaturedProjects.vue
+++ src/components/FeaturedProjects.vue
@@ -7,21 +7,11 @@ const props = defineProps({
         required: true,
     },
 })
-
-console.log(props.projects)
 </script>
 <template>
     <div class='featured-projects'>
         <div class="project-grid">
-            <div v-for="project in projects" class='card featured-project'>
-                <div class='img-wrap'>
-                    <img src="../assets/img/ruta1x1.jpg" alt="">
-                </div>
-                <div class="text">
-                    <span class="title">Responsive Design</span>
-                    <span class="description">Creating dazzling, responsive experiences for all devices.</span>
-                </div>
-            </div>
+            <CardFeaturedProject v-for="project in projects" :project="project"/>
         </div>
     </div>
 </template>
--- src/components/cards/CardFeaturedProject.vue
+++ src/components/cards/CardFeaturedProject.vue
@@ -9,18 +9,15 @@ const props = defineProps({
 })
 
 </script>
+
 <template>
-    <div v-if="project" class="card featured-project">
-        <div class="image">
-            <img :src="getThumbnailUrl(project?.mainImage)" alt="">
+    <div class='card featured-project'>
+        <div class='img-wrap'>
+            <img :src="getThumbnailUrl(project?.mainImage)" :alt="`Image of ${project.title}`">
         </div>
         <div class="text">
-            <h3>{{ project?.title }}</h3>
-            <p>{{ project?.description[0].children[0].text }}</p>
-            <div class="links">
-                <a v-if="project.url" :href="project.url" target="_blank">View Project</a>
-                <a v-if="project.repository" :href="project.repository" target="_blank">View Code</a>
-            </div>
+            <span class="title">{{ project.title }}</span>
+            <span class="description">{{ project?.description[0].children[0].text }}</span>
         </div>
     </div>
 </template>
