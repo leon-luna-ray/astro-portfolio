755c52117202e81f1dd38fff59788110a454a855
Ray Luna <leon.luna.ray@gmail.com>
2023-07-02 15:07:40 -0700

    pull profile image from cms

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { getLargeUrl } from '../lib/images';
+
+const props = defineProps({
+    profileImage: {
+        type: Object,
+        required: true,
+    },
+})
+
+
 const slideBgColor = (index: number) => {
-  return index % 2 === 0 ? 'brown' : 'black';
+    return index % 2 === 0 ? 'brown' : 'black';
 }
 </script>
 
@@ -10,7 +21,8 @@ const slideBgColor = (index: number) => {
             <img src="../assets/img/sample2.jpg" alt="test img">
         </div>
         <div :class="['slide', slideBgColor(1)]">
-            <img src="../assets/img/ruta1x1.jpg" alt="test img">
+            <!-- <img src="../assets/img/ruta1x1.jpg" alt="test img"> -->
+            <img :src="getLargeUrl(profileImage)" alt="Profile Image">
         </div>
         <div :class="['slide', slideBgColor(2)]">
             <img src="../assets/img/sample1.jpg" alt="test img">
