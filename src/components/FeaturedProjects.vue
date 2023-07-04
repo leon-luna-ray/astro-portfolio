537629633ab9e537d74ee5d92eb181e9b9ea31e0
Ray Luna <leon.luna.ray@gmail.com>
2023-07-03 21:19:49 -0700

    create interfaces

--- src/components/FeaturedProjects.vue
+++ src/components/FeaturedProjects.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import CardFeaturedProject from './cards/CardFeaturedProject.vue';
+import Project from '../interfaces/Project';
 
 const props = defineProps({
-    projects: {
-        type: Array,
-        required: true,
-    },
-})
-
-
+  projects: {
+    type: Array as () => Project[],
+    required: true,
+  },
+});
 </script>
 <template>
     <div id="featured-projects" class='featured-projects'>
@@ -16,7 +15,7 @@ const props = defineProps({
             <h3>Featured Projects</h3>
         </div>
         <div class="project-grid">
-            <CardFeaturedProject v-for="project in projects" :project="project"/>
+            <CardFeaturedProject v-for="project in projects" :project="project" />
         </div>
     </div>
 </template>
