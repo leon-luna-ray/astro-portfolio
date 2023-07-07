9d0ec051bfb5c6596629c52c5a95e30d31d98637
Ray Luna <leon.luna.ray@gmail.com>
2023-07-06 23:20:22 -0700

    add skill descriptions

--- src/components/TechList.vue
+++ src/components/TechList.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { defineProps, ref } from 'vue';
+
+import PortableText from '../components/PortableText.vue'
+
 const props = defineProps({
     title: {
         type: String,
@@ -8,13 +12,23 @@ const props = defineProps({
         type: Array,
         required: true,
     },
-})
+});
+
+const expandedItem = ref(null);
+
+function toggleItem(item) {
+    expandedItem.value = (expandedItem.value === item ? null : item);
+}
+console.log(props.items)
 </script>
 <template>
     <div class="tech-list">
         <h3 v-if="title" class="title">{{ title }}</h3>
         <ul class="link-list">
-            <li v-for="item in items">{{ item.title }}</li>
+            <li v-for="item in items">
+                <span>{{ item.title }}</span>
+                <PortableText :data="item.description" class="description"/>
+            </li>
         </ul>
     </div>
-</template>
+</template>x
