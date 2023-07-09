6cba8227b7d007dde182574e6532286a8ee01516
Ray Luna <leon.luna.ray@gmail.com>
2023-07-09 14:48:41 -0700

    add image gallery component

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { getThumbnailUrl } from '../lib/images';
+
+const props = defineProps({
+    images: {
+        type: Array,
+        required: true,
+    },
+});
+</script>
+<template>
+    <div class="image-gallery">
+        <div v-for="(image, index) in images" class="img-wrap">
+            <img :src="getThumbnailUrl(image)" :alt="`Project Screenshot ${index + 1}`">
+        </div>
+    </div>
+</template>
