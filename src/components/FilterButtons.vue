30c410693729eddbc79ea9be4670ec2ea0493b96
Ray Luna <leon.luna.ray@gmail.com>
2023-12-23 17:32:51 -0800

    add filter buttons

---
+++ src/components/FilterButtons.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-if="items.length" class="container flex flex-wrap">
+        <div v-for="item in items" class="rounded-lg border border-neutral-brown px-[1rem] py-[0.2rem] m-[0.2rem]">{{ item.title }}</div>
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    items: {
+        type: Array,
+        required: true,
+    }
+})
+</script>
