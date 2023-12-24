f08c85d2dcfbcc0d25a282ec65b4689bc6b064d2
Ray Luna <leon.luna.ray@gmail.com>
2023-12-23 21:10:04 -0800

    update dark mode styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,37 @@
+<template>
+    <header id="header" :class="['dark:text-primary-yellow',bottomPadding]">
+        <div class='top-label hover:text-inherit'>
+            <div class='flex-grow bg-black h-[2px] hidden md:block'></div>
+            <div class="px-4 py-[1rem] md:py-0 flex justify-center gap-x-[0.5rem]">
+                <a v-if="isMobile && path.length" href="/" class="text-center font-semibold hover:text-white">
+                    <div class="site">
+                        <!-- Todo refactor hard coded values -->
+                        <div v-if="path.length > 1" class="inner">
+                            <span>Ray Luna</span>
+                            <span>-</span>
+                            <span>Developer Portfolio</span>
+                        </div>
+                        <div v-else class="flex gap-x-[0.3rem] flex-col">
+                            <span>Ray Luna</span>
+                            <span>Developer Portfolio</span>
+                        </div>
+                    </div>
+                    <span v-if="!isHome" class="capitalize">
+                        {{ getTitle(path[path.length - 1]) }}
+                    </span>
+                </a>
+                <div v-else-if="path.length" v-for="(item, index) in path" class="flex">
+                    <a v-if="index < path.length - 1" :href="path[index]" class="text-center font-bold text capitalize">{{
+                        getTitle(item) }}</a>
+                    <span v-else class="text-center font-bold text capitalize">{{ getTitle(item) }}</span>
+                    <span v-if="index < path.length - 1" class="hidden md:block pl-2 hidden md:block"> - </span>
+                </div>
+            </div>
+            <div class='divider'></div>
+        </div>
+    </header>
+</template>
+
 <script lang="ts" setup>
 import { computed, ref, watch } from 'vue';
 import { useWindowScroll, useMediaQuery } from '@vueuse/core'
@@ -54,35 +88,3 @@ const getTitle = (path: string): string => {
 // Watchers
 watch(y, handleScroll)
 </script>
-<template>
-    <header id="header" :class="bottomPadding">
-        <div class='top-label hover:text-inherit'>
-            <div class='divider'></div>
-            <div class="label-text">
-                <a v-if="isMobile && path.length" href="/" class="mobile">
-                    <div class="site">
-                        <!-- Todo refactor hard coded values -->
-                        <div v-if="path.length > 1" class="inner">
-                            <span>Ray Luna</span>
-                            <span>-</span>
-                            <span>Developer Portfolio</span>
-                        </div>
-                        <div v-else class="inner home">
-                            <span>Ray Luna</span>
-                            <span>Developer Portfolio</span>
-                        </div>
-                    </div>
-                    <span v-if="!isHome" class="page-title">
-                        {{ getTitle(path[path.length - 1]) }}
-                    </span>
-                </a>
-                <div v-else-if="path.length" v-for="(item, index) in path" class="path flex">
-                    <a v-if="index < path.length - 1" :href="path[index]" class="text-item">{{ getTitle(item) }}</a>
-                    <span v-else class="text-item">{{ getTitle(item) }}</span>
-                    <span v-if="index < path.length - 1" class="dash pl-2 hidden md:block"> - </span>
-                </div>
-            </div>
-            <div class='divider'></div>
-        </div>
-    </header>
-</template>
