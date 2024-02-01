9868026279fa96ea2b40ad05c4e416378457488b
Ray Luna <leon.luna.ray@gmail.com>
2024-01-31 21:47:23 -0800

    update image-text-tout

--- src/components/ImageTextTout.vue
+++ src/components/ImageTextTout.vue
@@ -12,17 +12,16 @@ const props = defineProps({
 });
 </script>
 <template>
-    <div class='overflow-hidden'>
-        <div class="container grid md:grid-cols-2">
-            <div class='text-center md:text-left flex justify-center flex-col'>
-                <span v-if="label" class='font-semibold'>{{ label }}</span>
-                <p class="font-monoton text-[2rem] md:text-[3.125rem] pt-[1rem] md:pt-[2.5rem]">{{ text }}</p>
+    <div class="container grid md:grid-cols-2">
+
+        <div class='relative flex justify-center items-center items-center aspect-square'>
+            <div class="flex justify-center items-center aspect-square rounded-3xl p-[2rem] max-w-[460px] w-fit bg-black">
+                <slot />
             </div>
-            <div class='hidden md:flex relative justify-end items-center h-[98%] aspect-square overflow-hidden'>
-                <!-- <img src="/img/spinner.png" alt="decoration"> -->
-                <!-- <DiscoBall client:load /> -->
-                <slot/>
-            </div>
+        </div>
+        <div class='text-center md:text-left flex justify-center flex-col'>
+            <span v-if="label" class='font-semibold'>{{ label }}</span>
+            <p class="font-monoton text-[2rem] md:text-[3.125rem] pt-[1rem] md:pt-[2.5rem]">{{ text }}</p>
         </div>
     </div>
 </template>
