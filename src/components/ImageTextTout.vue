97b1cee7255a576ea5c9b501fd425d48fa7f34b6
Ray Luna <leon.luna.ray@gmail.com>
2024-02-05 19:16:46 -0800

    update image text layout

--- src/components/ImageTextTout.vue
+++ src/components/ImageTextTout.vue
@@ -12,14 +12,13 @@ const props = defineProps({
 });
 </script>
 <template>
-    <div class="container grid md:grid-cols-2">
-
-        <div class='relative flex justify-center items-center items-center aspect-square'>
+    <div class="container grid md:grid-cols-5 gap-x-[4rem] md:py-[5rem]">
+        <div class='md:col-span-2 relative flex justify-center items-center aspect-square'>
             <div class="flex justify-center items-center aspect-square rounded-3xl p-[2rem] max-w-[460px] w-fit bg-black">
                 <slot />
             </div>
         </div>
-        <div class='text-center md:text-left flex justify-center flex-col'>
+        <div class='md:col-span-3 text-center md:text-left flex justify-center flex-col'>
             <span v-if="label" class='font-semibold'>{{ label }}</span>
             <p class="font-monoton text-[2rem] md:text-[3.125rem] pt-[1rem] md:pt-[2.5rem]">{{ text }}</p>
         </div>
