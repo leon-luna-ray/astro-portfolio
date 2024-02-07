100babcef595c6e8a659d066d6e12ca2e543e2ce
Ray Luna <leon.luna.ray@gmail.com>
2024-02-06 18:53:58 -0800

    normalize home page sections

---
+++ src/components/TextBlock.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex-col-1 container">
+        <slot name="title"></slot>
+        <div class="richtext" v-if="text && isRichtext" v-html="sanityPortableText(text)"></div>
+        <div class="richtext" v-if="text">{{ text }}</div>
+    </div>
+</template>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { sanityPortableText } from '../lib/text';
+
+const props = defineProps({
+    text: {
+        type: [String, Object],
+        required: false
+    },
+});
+
+const isRichtext = computed(() => {
+    return typeof props.text === 'object';
+});
+</script>
