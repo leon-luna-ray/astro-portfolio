d2e0d5929f9e6a0590ce8191812a2e823829ed98
Ray Luna <leon.luna.ray@gmail.com>
2024-02-09 20:46:07 -0800

    refactor activity log styles

--- src/components/ActivityLog.vue
+++ src/components/ActivityLog.vue
@@ -9,11 +9,11 @@ const props = defineProps({
 });
 </script>
 <template>
-    <ul class="activity-log">
+    <ul class="flex-col-05">
         <li v-for="item in items">
-            <a :href="item.html_url" target="_blank" class="hover:text-neutral-brown">
-                <p v-if="item.commit?.message" class="message">{{ item.commit?.message }}</p>
-                <span class="date">{{ formatDate(item.commit.author.date) }}</span>
+            <a :href="item.html_url" target="_blank" class="hover:text-neutral-brown grid grid-cols-6 gap-x-[2rem] no-underline hover:text-white">
+                <p v-if="item.commit?.message" class="col-span-4 xl:col-span-5">{{ item.commit?.message }}</p>
+                <span class="col-span-2 xl:col-span-1 flex justify-end">{{ formatDate(item.commit.author.date) }}</span>
             </a>
         </li>
     </ul>
