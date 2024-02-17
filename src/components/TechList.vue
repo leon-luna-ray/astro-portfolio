18fd3904fd523ca4034e8cf07d38bc3dfd576d65
Ray Luna <leon.luna.ray@gmail.com>
2024-02-16 21:19:22 -0800

    update mobile styles

--- src/components/TechList.vue
+++ src/components/TechList.vue
@@ -39,8 +39,8 @@ onMounted(() => {
             <li v-for="(item, index) in items" class="hover:cursor-pointer flex-col-1">
                 <div class="flex justify-between pb-0 items-center text-[1rem]" @click="setExpandedItem(index)">
                     <span>{{ item.title }}</span>
-                    <!-- <IconChevronUp v-if="expandedItem === index" class="h-[1rem] up" aria-hidden="true"/>
-                    <IconChevronDown v-else class="h-[1rem] down" aria-hidden="true"/> -->
+                    <IconChevronUp v-if="expandedItem === index" class="h-[1rem] up" aria-hidden="true"/>
+                    <IconChevronDown v-else class="h-[1rem] down" aria-hidden="true"/>
                 </div>
                 <p v-if="expandedItem === index" class="text-[0.875rem] font-light" @click="setExpandedItem(null)">{{ item.description }}
                 </p>
