ace9168721a1297c2f0d8e1a5e22fa6b2c17761a
Ray Luna <leon.luna.ray@gmail.com>
2024-02-16 19:16:30 -0800

    add organizaitons to project detail page

--- src/components/cards/CardOrganization.vue
+++ src/components/cards/CardOrganization.vue
@@ -1,3 +1,11 @@
 <template>
-    <div>Card</div>
+    <a v-if="org.website" :href="org.website" target="_blank" class="no-underline flex gap-x-[1rem] items-center zoom-in-out">
+        <img :src="getThumbnailUrl(org.image)" :alt="`${org.title} logo`" class="w-12 h-12 rounded-full" />
+        <span class="text-[1.125rem]">{{ org.title }}</span>
+    </a>
 </template>
+<script setup lang="ts">
+import { getThumbnailUrl } from '../../lib/images';
+import type { Organization } from '../../interfaces/Organization';
+const props = defineProps<{ org: Organization; }>();
+</script>
