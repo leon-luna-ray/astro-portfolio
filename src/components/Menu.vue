fdae84934537030d1f3f3738d9b243173818279f
Ray Luna <leon.luna.ray@gmail.com>
2024-02-26 20:52:10 -0800

    setup menu component and store

---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-if="$isMenuOpen" class="bg-red-300 fixed top-0 right-0 h-screen z-10">
+        <h1>Menu</h1>
+        <h2 @click="isMenuOpen.set(!$isMenuOpen)">X</h2>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { isMenuOpen } from '../stores/menu';
+import { useStore } from '@nanostores/vue';
+
+const $isMenuOpen = useStore(isMenuOpen);
+</script>
