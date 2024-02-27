f6bc57b3a89babe47d0e947451c13784d9ba09a3
Ray Luna <leon.luna.ray@gmail.com>
2024-02-26 21:18:11 -0800

    translate css to tailwind

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,13 +1,19 @@
 <template>
-    <div v-if="$isMenuOpen" class="bg-red-300 fixed top-0 right-0 h-screen z-10">
+    <div
+        :class="[isVisible, 'fixed top-0 right-[-100vw] md:right-[-50vw] w-[100vw] md:w-[50vw] h-screen bg-theme z-10 transition-transform duration-300 ease-in-out']">
+        <button @click="isMenuOpen.set(!$isMenuOpen)">X</button>
         <h1>Menu</h1>
-        <h2 @click="isMenuOpen.set(!$isMenuOpen)">X</h2>
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { isMenuOpen } from '../stores/menu';
 import { useStore } from '@nanostores/vue';
 
 const $isMenuOpen = useStore(isMenuOpen);
+
+const isVisible = computed(() => ({
+    'translate-x-[-50vw]': $isMenuOpen.value,
+}))
 </script>
