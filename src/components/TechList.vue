00836633e6a049577af6a06955d48a4961af98dd
Ray Luna <leon.luna.ray@gmail.com>
2024-06-22 10:04:04 -0700

    refine on click behavior

--- src/components/TechList.vue
+++ src/components/TechList.vue
@@ -43,11 +43,10 @@ onMounted(() => {
                     <IconChevronUp v-if="expandedItem === index" class="h-[1rem] up" aria-hidden="true" />
                     <IconChevronDown v-else class="h-[1rem] down" aria-hidden="true" />
                 </div>
-                <div v-if="expandedItem === index" class="text-[0.875rem] font-light flex-col-1"
-                    @click="setExpandedItem(null)">
-                    <p>{{ item.description }}</p>
+                <div v-if="expandedItem === index" class="text-[0.875rem] font-light flex-col-1">
+                    <p @click="setExpandedItem(null)">{{ item.description }}</p>
                     <a :href="`/projects/technologies/${item.slug.current}`"
-                        class="text-[0.75rem] uppercase font-[500] hover:underline flex gap-[0.5rem] items-center">
+                        class="text-[0.75rem] text-chocolate dark:text-beige uppercase font-[500] hover:underline flex gap-[0.5rem] items-center tracking-[0.01rem]">
                         <span>{{ item.title }} Projects </span>
                         <IconChevronRight class="h-[1rem]" />
                     </a>
