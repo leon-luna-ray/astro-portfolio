2800cc0fb4da6a11c6e8b0b7c5635072eda87e65
Ray Luna <leon.luna.ray@gmail.com>
2024-06-22 11:20:32 -0700

    update filter button styles

--- src/components/FilterButtons.vue
+++ src/components/FilterButtons.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import IconClose from '../components/icons/IconClose.vue';
+
+import type { Tag } from '../interfaces/Tag';
+
 const props = defineProps({
     items: {
-        type: Array,
+        type: Array as PropType<Tag[]>, // Specify that items is an array of Tag
         required: true,
     },
     activeItem: {
@@ -13,16 +17,23 @@ const props = defineProps({
 const getHref = (slug: string) => {
     return slug === props.activeItem ? '/projects' : `/projects/technologies/${slug}`;
 }
+const getAriaLabel = (item: Tag) => { // Use Tag type for item
+    if (props.activeItem === item.slug.current) {
+        return `Click to view all projects`;
+    }
+    return `Click to view ${item.title} projects`;
+}
 </script>
 
 <template>
-    <div v-if="items.length" class="container pt-[2rem]">
-        <div class=" flex flex-wrap justify-center ">
-
+    <section v-if="items.length" class="container pt-[2rem]">
+        <div class="flex flex-wrap justify-center">
             <a v-for="item in items" :key="item._id" :href="getHref(item.slug.current)"
-                :class="[{ 'active': item.slug.current === activeItem }, 'filter-btn']">
-                {{ item.title }}
+                :class="[{ 'active': item.slug.current === activeItem }, 'filter-btn']"
+                :aria-label="getAriaLabel(item)">
+                <span>{{ item.title }}</span>
+                <IconClose v-if="item.slug.current === activeItem" />
             </a>
         </div>
-    </div>
+    </section>
 </template>
